05a55ee1bbdafa4f6599be75d96c82fb8abdb93d
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-09-01 17:00:25 +0200

    Added bootstrap and navbar with routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonPageComponent } from './pages/pokemon-page/pokemon-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
-    PokemonPageComponent
+    PokemonPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
