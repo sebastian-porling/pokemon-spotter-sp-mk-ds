1d3f858fe5392b97656d4f9ea7462916a92bf3d4
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-09-01 16:28:28 +0200

    Initial setup and pokemon-page

---
+++ frontend/src/app/mock-data/mock-pokemons.ts
@@ -0,0 +1,20 @@
+import { Pokemon } from "../models/pokemon";
+
+export const POKEMONS: Pokemon[] = [
+    {id: 1, name: "Pikachu", sprite:"img", spotted: 3},
+    {id: 2, name: "Bulbasaur", sprite:"img",  spotted: 13},
+    {id: 3, name: "Pikachu", sprite:"img", spotted: 2},
+    {id: 4, name: "wobbuffet", sprite:"img", spotted: 16},
+    {id: 5, name: "girafarig", sprite:"img", spotted: 4},
+    {id: 6, name: "forretress", sprite:"img", spotted: 1},
+    {id: 7, name: "pineco", sprite:"img", spotted: 31},
+    {id: 8, name: "dunsparce", sprite:"img", spotted: 23},
+    {id: 9, name: "gligar", sprite:"img", spotted: 38},
+    {id: 10, name: "steelix", sprite:"img", spotted: 1},
+    {id: 11, name: "snubbull", sprite:"img", spotted: 123},
+    {id: 12, name: "granbull", sprite:"img", spotted: 33},
+    {id: 13, name: "qwilfish", sprite:"img", spotted: 309},
+    {id: 14, name: "scizor", sprite:"img", spotted: 1312},
+    {id: 15, name: "shuckle", sprite:"img", spotted: 3123},
+    {id: 16, name: "heracross", sprite:"img", spotted: 813},
+]
