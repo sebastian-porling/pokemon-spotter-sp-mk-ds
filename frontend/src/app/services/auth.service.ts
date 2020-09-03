b7760e96fb1c35c5209420f63bcfb0d29c11dcef
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-03 22:52:09 +0200

    feat: added registration services

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { AngularFireAuth } from '@angular/fire/auth';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { auth } from 'firebase/app';
 
 @Injectable({
@@ -10,15 +12,32 @@ export class AuthService {
 
   public user: Observable<firebase.User>;
 
-  constructor(public authNg: AngularFireAuth) {
+  constructor(
+    public authNg: AngularFireAuth,
+    private http: HttpClient,
+    private router: Router
+    ) {
     this.user = authNg.authState;
   }
-  public signUp(email, password): void {}
+  public signUp(user): void {
+    console.log(user);
+    this.http.post('http://localhost:3000/auth/register', user,
+    {'headers': { 'content-type': 'application/json'} }).subscribe(
+      data => {
+        this.signIn(user.email, user.password);
+      },
+      error => console.log('oops', error)
+      );
+  }
 
   public signIn(email, password): void{
-    this.authNg.signInWithEmailAndPassword(email, password);
+    this.authNg.signInWithEmailAndPassword(email, password).then(() => {
+      this.router.navigate(['userstartpage']);
+    });
+
   }
   public signOut(): void{
     this.authNg.signOut();
+    this.router.navigate(['']);
   }
 }
