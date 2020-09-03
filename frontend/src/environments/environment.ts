457fca7acb0902070c6bdaefaaa969c825bc80b7
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-03 15:02:43 +0200

    feat: added firebase, login, started on register

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD1B29SQXLJIqtbEHyOSlvVy2QDL9YjH3Q",
+    authDomain: "pokemon-spotter.firebaseapp.com",
+    databaseURL: "https://pokemon-spotter.firebaseio.com",
+    projectId: "pokemon-spotter",
+    storageBucket: "pokemon-spotter.appspot.com",
+    messagingSenderId: "877105547164",
+    appId: "1:877105547164:web:ac0a6455c3ee285de86a59",
+    measurementId: "G-X5Y9XWL7PL"
+  }
 };
 
 /*
